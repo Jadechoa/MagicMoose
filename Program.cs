7a772b684980e753432883b8584a370e3cb48c7d
Jade Choate <jadeschoate@gmail.com>
2023-02-23 16:03:30 -0600

    Randomize Replies

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MagicMoose
+Main();
+
+ void Main()
 {
-    class Program
+
+    Console.WriteLine("Welcome to the Magic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    MooseQuestion();
+
+    void MooseSays(string message)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
     }
-}
+
+    List<string> replies = new List<string>()
+{
+   " As I see it, yes.", "Ask again later.", "Better not tell you now.", "It is certain.",
+"It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.",
+"Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes, definitely.", "You may rely on it."
+};
+
+    void MooseQuestion()
+    {
+        Console.Write($"Please Ask a question. /n");
+        string question = Console.ReadLine();
+    }
+
+    Random r = new Random();
+    MooseSays(replies[r.Next(0, replies.Count)]);
+ }
+
